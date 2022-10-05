3b73d65316d7feede9717089572296cd6521e1fd
edivanmd <edivanmd@gmail.com>
2022-10-05 16:38:52 +1100

    Navbar adjustments

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,14 +11,12 @@ const Navbar = () => {
   const [color, setColor] = useState('transparent');
   const [textColor, setTextColor] = useState('white');
 
-  const Menu = ({ isMobile }) => (
-     <ul className={ isMobile ? '' : 'hidden sm:flex' } style={{color: !isMobile && `${textColor}`}}>
-      <li className={isMobile ? 'p-4 hover:text-gray-500 text-4xl' : 'p-4 hover:text-gray-500'}><Link href='/'>Home</Link></li>
-      <li className={isMobile ? 'p-4 hover:text-gray-500 text-4xl' : 'p-4 hover:text-gray-500'}><Link href='/#gallery'>Gallery</Link></li>
-      <li className={isMobile ? 'p-4 hover:text-gray-500 text-4xl' : 'p-4 hover:text-gray-500'}><Link href='/#portfolio'>Work</Link></li>
-      <li className={isMobile ? 'p-4 hover:text-gray-500 text-4xl' : 'p-4 hover:text-gray-500'}><Link href='/#contact'>Contact</Link></li>
-    </ul>
-  );
+  const navLinks = [
+    {name: 'Home', path: '/'},
+    {name: 'Gallery', path: '/#gallery'},
+    {name: 'Portfolio', path: '/#portfolio'},
+    {name: 'Contact', path: '/#contact'},
+  ]
 
   const handleNavbar = () => {
     setNav(!nav);
@@ -42,7 +40,13 @@ const Navbar = () => {
       <div className={style.navbarInner}>
         <Link href='/'><h1 style={{color: `${textColor}`}} className={style.navbarLogo}>Côncavo</h1></Link>
         <div>
-          <Menu isMobile={false} />
+          <ul className='hidden sm:flex' style={{color: `${textColor}`}}>
+            {navLinks.map((link, index) => (
+              <li key={index} className='p-4 hover:text-gray-500'>
+                <Link href={link.path}>{link.name}</Link>
+              </li>
+            ))}
+          </ul>
         </div>
 
         {/* Mobile Button */}
@@ -56,7 +60,13 @@ const Navbar = () => {
             ? 'sm:hidden absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300'
             : 'sm:hidden absolute top-0 left-[100%] right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300'
         }>
-          <Menu isMobile={true} />
+          <ul>
+            {navLinks.map((link, index) => (
+              <li key={index} className='p-4 hover:text-gray-500 text-4xl'>
+                <Link href={link.path}>{link.name}</Link>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </nav>
