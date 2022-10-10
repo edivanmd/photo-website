9c510a49b4ee1308efdbf3919043e9b944748b91
edivanmd <edivanmd@gmail.com>
2022-10-10 14:38:35 +1100

    Projects section

--- components/Project.jsx
+++ components/Project.jsx
@@ -3,15 +3,25 @@ import React from 'react'
 
 const Project = ({ dataProject }) => {
   return (
-    <li className='w-full h-full first:col-span-2 first:md:col-span-3 first:row-span-2'>
-      <Image
-          src={dataProject.image}
-          alt='/'
-          layout='responsive'
-          width='677'
-          height='451'
-        />
-        {/* <span>{dataProject.title}</span> */}
+    <li className='relative w-full h-full first:col-span-2 first:md:col-span-3 first:row-span-2'>
+      { dataProject.main === 'true'
+        ? <Image
+            src={dataProject.image}
+            alt='/'
+            layout='responsive'
+            width='677'
+            height='451'
+          /> 
+        : <Image
+            src={dataProject.image}
+            alt='/'
+            layout='responsive'
+            width='215'
+            height='217'
+            objectFit='cover'
+          />
+      }
+      <p className='absolute z-10 bottom-0 rounded-t p-1 w-full bg-gradient-to-t from-sky-500 to-indigo-500'>{dataProject.title}</p>
     </li>
   )
 }
--- pages/projects.jsx
+++ pages/projects.jsx
@@ -5,19 +5,28 @@ import Project from '../components/Project';
 const dataProjects = [
   {
     title: 'Patagonia - Agentina',
-    image: 'https://images.unsplash.com/photo-1557543878-042c9857f39e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80'
+    image: 'https://images.unsplash.com/photo-1557543878-042c9857f39e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80',
+    main: 'true'
   },
   {
     title: 'Sydney - Australia',
-    image: 'https://images.unsplash.com/photo-1598948485421-33a1655d3c18?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80'
+    image: 'https://images.unsplash.com/photo-1598948485421-33a1655d3c18?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80',
+    main: 'false'
   },
   {
     title: 'Rio de Janeiro - Brazil',
-    image: 'https://images.unsplash.com/photo-1483729558449-99ef09a8c325?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+    image: 'https://images.unsplash.com/photo-1483729558449-99ef09a8c325?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    main: 'false'
   },
   {
     title: 'London - UK',
-    image: 'https://images.unsplash.com/photo-1513635269975-59663e0ac1ad?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+    image: 'https://images.unsplash.com/photo-1513635269975-59663e0ac1ad?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    main: 'false'
+  },
+  {
+    title: 'Paris - France',
+    image: 'https://images.unsplash.com/photo-1431274172761-fca41d930114?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    main: 'false'
   }
 ]
 
